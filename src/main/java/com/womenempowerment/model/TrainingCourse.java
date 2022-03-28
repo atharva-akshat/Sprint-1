cfcf548c7da501da69516bd5961c1e6b19d2e5e9
Ankit Mishra <43310904+AnkitMishra19007@users.noreply.github.com>
2022-03-28 21:07:07 +0530

    making entity class

--- src/main/java/com/womenempowerment/model/TrainingCourse.java
+++ src/main/java/com/womenempowerment/model/TrainingCourse.java
@@ -1,12 +1,26 @@
 package com.womenempowerment.model;
-
 import java.time.LocalDate;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="trainingcourse")
 public class TrainingCourse {
-	Integer trainingCourseId;
-	String	courseName;
-	String courseDuration;
-	LocalDate	startingDate;
-	LocalDate	endingDate;
-	String	courseCompletionStatus;
+	@Id
+	@Column(length = 4)
+	private Integer trainingCourseId;
+	@Column(length = 20)
+	private String	courseName;
+	@Column(length = 10)
+	private String courseDuration;
+	@Column
+	private LocalDate	startingDate;
+	@Column
+	private LocalDate	endingDate;
+	@Column(length = 10)
+	private String	courseCompletionStatus;
 }
+
