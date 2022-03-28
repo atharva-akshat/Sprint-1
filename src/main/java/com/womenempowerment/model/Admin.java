00582b48010c0b3d0ed48468667ec68ccb2ea65d
Ankit Mishra <43310904+AnkitMishra19007@users.noreply.github.com>
2022-03-28 21:13:52 +0530

    Update Admin.java

--- src/main/java/com/womenempowerment/model/Admin.java
+++ src/main/java/com/womenempowerment/model/Admin.java
@@ -15,6 +15,26 @@ public class Admin {
 	private	String adminUsername;
 	@Column(length = 10)
 	private	String adminPassword;
-
+	
+	//Getters and setters
+	public int getAdminId() {
+		return adminId;
+	}
+	public void setAdminId(int adminId) {
+		this.adminId = adminId;
+	}
+	public String getAdminUsername() {
+		return adminUsername;
+	}
+	public void setAdminUsername(String adminUsername) {
+		this.adminUsername = adminUsername;
+	}
+	public String getAdminPassword() {
+		return adminPassword;
+	}
+	public void setAdminPassword(String adminPassword) {
+		this.adminPassword = adminPassword;
+	}
+	
 }
 
