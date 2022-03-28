5889cae4ce600a5ed42dad19b171cda8b933722b
Ankit Mishra <43310904+AnkitMishra19007@users.noreply.github.com>
2022-03-28 21:06:21 +0530

    making entity class

--- src/main/java/com/womenempowerment/model/User.java
+++ src/main/java/com/womenempowerment/model/User.java
@@ -1,7 +1,19 @@
 package com.womenempowerment.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="user")
 public class User {
+	@Id
+	@Column(length = 4)
 	private int loginId;
+	@Column(length = 10)
 	private String userName;
+	@Column(length = 10)
 	private String userPassword;
 }
+
