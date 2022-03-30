03622cee7532c4a3e8955f432aab865bb7207b32
Atharva Akshat <atharv.akshat@outlook.com>
2022-03-30 15:16:15 +0530

    Adding Exception Handler to utils

--- src/main/java/com/womenempowerment/utils/MainExceptionHandler.java
+++ src/main/java/com/womenempowerment/utils/MainExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.womenempowerment.utils;
 
 import com.womenempowerment.exception.InvalidUserException;
+import com.womenempowerment.exception.NGONotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -11,6 +12,11 @@ public class MainExceptionHandler {
     @ExceptionHandler(value = InvalidUserException.class)
     public ResponseEntity<Object> exception(InvalidUserException ex){
         System.out.println(ex.getMessage());
-        return new ResponseEntity<Object>(ex.getMessage(), HttpStatus.NOT_FOUND);
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
+    }
+    @ExceptionHandler(value = NGONotFoundException.class)
+    public ResponseEntity<Object> exception(NGONotFoundException ex){
+        System.out.println(ex.getMessage());
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
     }
 }
