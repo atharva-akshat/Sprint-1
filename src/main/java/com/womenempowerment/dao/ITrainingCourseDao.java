f038d97d82c9f0b5ef31ec43c1b2b149b28ec8a7
Ankit Mishra <ankitmishra19007@gmail.com>
2022-03-30 11:57:51 +0530

    query to get course by name
    still need some changes

--- src/main/java/com/womenempowerment/dao/ITrainingCourseDao.java
+++ src/main/java/com/womenempowerment/dao/ITrainingCourseDao.java
@@ -3,9 +3,12 @@ package com.womenempowerment.dao;
 
 import com.womenempowerment.entity.TrainingCourse;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ITrainingCourseDao extends JpaRepository<TrainingCourse, Integer> {
-
+	@Query(value = "SELECT * FROM trainingcourse tc WHERE tc.course_name like %?1%", 
+			  nativeQuery = true)
+	public TrainingCourse viewbyCourseName(String name);
 }
