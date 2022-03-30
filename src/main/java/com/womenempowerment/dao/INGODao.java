a2166ba14cd68e765c194334596b6bae3e10f0a2
tejodeep122 <71459850+tejodeep122@users.noreply.github.com>
2022-03-30 11:58:36 +0530

    adding Ngoimpl and  ngoDao

--- src/main/java/com/womenempowerment/dao/INGODao.java
+++ src/main/java/com/womenempowerment/dao/INGODao.java
@@ -1,7 +1,7 @@
 package com.womenempowerment.dao;
 
-import com.womenempowerment.entity.NGO;
 
+import com.womenempowerment.entity.NGO;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
