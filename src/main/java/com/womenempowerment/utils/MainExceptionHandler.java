de2a204d12aca642d17ed65d7f6581edfa71fd63
Atharva Akshat <atharv.akshat@outlook.com>
2022-03-30 17:27:33 +0530

    ExceptionHandler to utils

--- src/main/java/com/womenempowerment/utils/MainExceptionHandler.java
+++ src/main/java/com/womenempowerment/utils/MainExceptionHandler.java
@@ -1,7 +1,6 @@
 package com.womenempowerment.utils;
 
-import com.womenempowerment.exception.InvalidUserException;
-import com.womenempowerment.exception.NGONotFoundException;
+import com.womenempowerment.exception.*;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -16,6 +15,21 @@ public class MainExceptionHandler {
     }
     @ExceptionHandler(value = NGONotFoundException.class)
     public ResponseEntity<Object> exception(NGONotFoundException ex){
+        System.out.println(ex.getMessage());
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
+    }
+    @ExceptionHandler(value = TrainingCourseNotFoundException.class)
+    public ResponseEntity<Object> exception(TrainingCourseNotFoundException ex){
+        System.out.println(ex.getMessage());
+        return new ResponseEntity<Object>(ex.getMessage(), HttpStatus.NOT_FOUND);
+    }
+    @ExceptionHandler(value = FeedBackNotFoundException.class)
+    public ResponseEntity<Object> exception(FeedBackNotFoundException ex){
+        System.out.println(ex.getMessage());
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
+    }
+    @ExceptionHandler(value = FeedBackAlreadyExistsException.class)
+    public ResponseEntity<Object> exception(FeedBackAlreadyExistsException ex){
         System.out.println(ex.getMessage());
         return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
     }
