6fd2e0550dfb43c2e0bd0f5944498ad8ab7896ae
Rithish Jakkireddy <rithishjakkireddy@gmail.com>
2022-03-31 21:16:33 +0530

    Create SchemeNotFoundException.java

---
+++ src/main/java/com/womenempowerment/exception/SchemeNotFoundException.java
@@ -0,0 +1,9 @@
+package com.womenempowerment.exception;
+
+public class SchemeNotFoundException extends RuntimeException{
+	private static final long serialVersionUID=1L;
+	public SchemeNotFoundException() {
+		super("Scheme does not exist!");
+	}
+
+}
