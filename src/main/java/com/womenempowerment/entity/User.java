ac4ba2418c99180b60c8a69e5c07d038c07c7d64
Atharva Akshat <atharv.akshat@outlook.com>
2022-04-12 01:50:16 +0530

    changes

--- src/main/java/com/womenempowerment/entity/User.java
+++ src/main/java/com/womenempowerment/entity/User.java
@@ -18,7 +18,7 @@ public class User {
     @Size(min = 2, max = 15, message = "Username must be between 2 and 15 characters long")
     private String userName;
     @Column(length = 15)
-    @Pattern(regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&-+=()])(?=\\S+$).{4,15}$", message = "Password must contain atleast one capital letter, one small letter, one digit, one special character and should be atleast 4 characters long")
+    @Pattern(regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#!$%^&-+=()])(?=\\S+$).{4,15}$", message = "Password must contain atleast one capital letter, one small letter, one digit, one special character and should be atleast 4 characters long")
     private String userPassword;
     
     public int getLoginId() {
